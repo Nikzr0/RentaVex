1a99230a105cdf31697afd39fa1584b30fa116f2
Nikzr0 <npepelov@gmail.com>
2024-04-18 05:05:41 +0300

    Creating global service and adding GetPageName() method.

---
+++ Services/RentaVex.Services.Data/GlobalService.cs
@@ -0,0 +1,25 @@
+﻿namespace RentaVex.Services.Data
+{
+    using System.Text.RegularExpressions;
+    using Microsoft.AspNetCore.Http;
+
+    public class GlobalService
+    {
+        public static string GetPageName(HttpContext httpContext)
+        {
+            string currentPageUrl = httpContext.Request.Path;
+            string pattern = @"/([^/]+)$";
+
+            Match match = Regex.Match(currentPageUrl, pattern);
+
+            if (match.Success)
+            {
+                return match.Groups[1].Value;
+            }
+            else
+            {
+                return "Products";
+            }
+        }
+    }
+}
